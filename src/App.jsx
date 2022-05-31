3d9d14d998772d95cfb6cb6b7add2ab3003bc517
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-05-31 16:40:50 -0300

    Create Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import './App.css';
+/* import './App.css'; */
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
 import Provider from './Context/Provider';
 import { Route, Routes } from 'react-router-dom';
 import PageLogin from './Pages/PageLogin';
+import PageSearch from './Pages/PageSearch';
+import PageAlbum from './Pages/PageAlbum';
+import PageFavorites from './Pages/PageFavorites';
+import PageProfile from './Pages/PageProfile';
+import PageProfileEdit from './Pages/PageProfileEdit';
+import PageNotFound from './Pages/PageNotFound';
 
 function App() {
   return (
     <Provider>
       <Routes>
         <Route exact path="/" element={ <PageLogin /> } />
+        <Route exact path="/search" element={ <PageSearch /> } />
+        <Route exact path="/album/:id" element={ <PageAlbum /> } />
+        <Route exact path="/favorites" element={ <PageFavorites /> } />
+        <Route exact path="/profile" element={ <PageProfile /> } />
+        <Route exact path="/profile/edit" element={ <PageProfileEdit /> } />
+        <Route exact path='*' element={ <PageNotFound /> } />
       </Routes>
     </Provider>
   );
---
+++ src/Pages/PageSearch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageSearch() {
+  return (
+    <div>
+      Search
+    </div>
+  );
+}
+
+export default PageSearch;
